66a6213c3b9d48e7ac2521a15d5c83758c71cd6d
carol <anycarolinif340@gmail.com>
2024-08-19 09:56:40 -0300

    ultimo passo

--- Equipe1/Modelos/Cliente.cs
+++ Equipe1/Modelos/Cliente.cs
@@ -5,6 +5,7 @@ namespace Modelos
 public class Cliente : Registro
 {
      [BsonId]
+public int Id { get; set; }
 public string Nome {get; set;}
 public string Enderecoo {get; set;}
 public string Email {get; set;}
